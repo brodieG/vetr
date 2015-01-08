d6ecda372764acb10de0ab48cb489ffeb99898ee
brodieG <brodieG@users.noreply.github.com>
2015-01-07 22:03:39 -0500

    removing old test files

--- tests/unitizer/checkargs2.testor.R
+++
@@ -1,8 +0,0 @@
-fun3 <- function(a, b, c=5L) {
-  check_args(
-    a=numeric(), 
-    b=c_and(c_or(is.integer(b), is.character(b)), is.matrix(b)), 
-    c=data.frame(a=character(), b=numeric(), c=factor(levels=c("moms", "pops")), stringsAsFactors=FALSE)
-  )
-}
-try(fun3(3, matrix(sample(1:20, 9), nrow=3), 1:3))
