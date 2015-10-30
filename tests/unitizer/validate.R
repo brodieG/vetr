d981feecd6626134ea90dab328732fee50c4cf8a
brodieG <brodieG@users.noreply.github.com>
2015-10-30 15:21:28 -0400

    testup

---
+++ tests/unitizer/validate.R
@@ -0,0 +1,72 @@
+library(validate)
+
+set.seed(1)
+unitizer_sect("Tokens Pass", {
+  validate(INT.1, 1)
+  validate(INT.1.POS, 1)
+  validate(INT.1.NEG, -1)
+  validate(INT.1.POS.STR, 1)
+  validate(INT.1.NEG.STR, -1)
+  validate(INT, -1:1)
+  validate(INT.POS, 0:3)
+  validate(INT.NEG, 0:-3)
+  validate(INT.POS.STR, 1:3)
+  validate(INT.NEG.STR, -(1:3))
+  validate(NUM.1, 1.44)
+  validate(NUM.1.POS, 1.44)
+  validate(NUM.1.NEG, -1.44)
+  validate(NUM, runif(5))
+  validate(NUM.POS, runif(5))
+  validate(NUM.NEG, -runif(5))
+  validate(CHR.1, "hello")
+  validate(CHR, letters)
+  validate(CPX, 1:10 + .5i)
+  validate(CPX.1, 1 + .5i)
+  validate(LGL, c(TRUE, FALSE))
+  validate(LGL.1, TRUE)
+} )
+unitizer_sect("Tokens Fail", {
+  validate(INT.1, 1.2)
+  validate(INT.1, 1:2)
+  validate(INT.1, NA_integer_)
+  validate(INT.1, Inf)
+  validate(INT.1.POS, -1)
+  validate(INT.1.POS, 1:2)
+  validate(INT.1.NEG, 1)
+  validate(INT.1.NEG, -(1:2))
+  validate(INT.1.POS.STR, 0)
+  validate(INT.1.NEG.STR, 0)
+  validate(INT, c(-1:1, NA_integer_))
+  validate(INT, letters)
+  validate(INT.POS, -(1:3))
+  validate(INT.NEG, 1:3)
+  validate(INT.POS.STR, 0:3)
+  validate(INT.NEG.STR, -(0:3))
+  validate(NUM.1, 1.44 + 1:2)
+  validate(NUM.1.POS, -runif(1) - 1)
+  validate(NUM.1.NEG, runif(1) + 1)
+  validate(NUM, c(NA_real_, 1))
+  validate(NUM, NULL)
+  validate(NUM.POS, -runif(5) - 1)
+  validate(NUM.NEG, runif(5) + 1)
+  validate(CHR.1, letters)
+  validate(CHR, list(1, 2, 3))
+  validate(CPX, list(1, 2, 3))
+  validate(CPX.1, list(1, 2, 3))
+  validate(LGL, NA)
+  validate(LGL, letters)
+  validate(LGL.1, 1:2 == 1:2)
+})
+unitizer_sect("Compound Expressions", {
+  validate(INT.1 || NULL, 1)    # Pass
+  validate(INT.1 || NULL, NULL) # Pass
+  validate(INT.1 || NULL, 1.4)  # Fail
+
+  validate((matrix(integer(), 0) && nrow(.) == ncol(.)) || NULL, matrix(1:16, nrow=4))  # Pass
+  validate((matrix(integer(), 0) && nrow(.) == ncol(.)) || NULL, matrix(1:16, nrow=2))  # Fail
+  validate((matrix(integer(), 0) && nrow(.) == ncol(.)) || NULL, matrix(runif(16), nrow=4))  # Fail
+
+
+})
+
+
