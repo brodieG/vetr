d830fc2e1a7e2a6d0095b8b9c84d6e07496debf4
brodieG <brodieG@users.noreply.github.com>
2017-05-23 21:48:31 -0400

    more validate->vetr
    Some questions about whether symb_sub worked properly

--- tests/unitizer/parse.R
+++ tests/unitizer/parse.R
@@ -1,70 +1,70 @@
 library(vetr)
 
 unitizer_sect("name_sub", {
-  validate:::name_sub(quote(.), quote(xyz))
-  validate:::name_sub(quote(.), quote(x + yz))   # Works, but may break parent fun
-  validate:::name_sub(quote(.), c(1:3))          # Works, but may break parent fun
-  validate:::name_sub(quote(..), quote(xyz))
-  validate:::name_sub(quote(...), quote(xyz))
-  validate:::name_sub(quote(.zzz), quote(xyz))
-  validate:::name_sub(quote(zzz.), quote(xyz))
-  validate:::name_sub(quote(zzz), quote(xyz))
-  validate:::name_sub(quote(a + b), quote(xyz))  # Does nothing
-  validate:::name_sub(quote(. + .), quote(xyz))  # Does nothing
-  validate:::name_sub(quote(.(zzz)), quote(xyz)) # Does nothing
-  validate:::name_sub("hello", quote(xyz))       # Does nothing
+  vetr:::name_sub(quote(.), quote(xyz))
+  vetr:::name_sub(quote(.), quote(x + yz))   # Works, but may break parent fun
+  vetr:::name_sub(quote(.), c(1:3))          # Works, but may break parent fun
+  vetr:::name_sub(quote(..), quote(xyz))
+  vetr:::name_sub(quote(...), quote(xyz))
+  vetr:::name_sub(quote(.zzz), quote(xyz))
+  vetr:::name_sub(quote(zzz.), quote(xyz))
+  vetr:::name_sub(quote(zzz), quote(xyz))
+  vetr:::name_sub(quote(a + b), quote(xyz))  # Does nothing
+  vetr:::name_sub(quote(. + .), quote(xyz))  # Does nothing
+  vetr:::name_sub(quote(.(zzz)), quote(xyz)) # Does nothing
+  vetr:::name_sub("hello", quote(xyz))       # Does nothing
 })
 unitizer_sect("remove parens", {
-  validate:::remove_parens(quote((a)))
-  validate:::remove_parens(quote(.(a)))
-  validate:::remove_parens(quote((((a)))))
-  validate:::remove_parens(quote((.((.(a))))))
-  validate:::remove_parens(quote((a) && .(a)))  # Nothing should be removed
+  vetr:::remove_parens(quote((a)))
+  vetr:::remove_parens(quote(.(a)))
+  vetr:::remove_parens(quote((((a)))))
+  vetr:::remove_parens(quote((.((.(a))))))
+  vetr:::remove_parens(quote((a) && .(a)))  # Nothing should be removed
 })
 unitizer_sect("parse", {
   x <- quote(.(.) && ((a)))
-  validate:::parse_validator(x, quote(arg_to_validate))
+  vetr:::parse_validator(x, quote(arg_to_validate))
   x # make sure unchanged from previous assignment
 
-  validate:::parse_validator(quote(FALSE), quote(arg_to_validate))
-  validate:::parse_validator(quote(((FALSE))), quote(arg_to_validate))
-  validate:::parse_validator(quote(((FALSE && ((TRUE))))), quote(arg_to_validate))
-  validate:::parse_validator(quote(.(FALSE)), quote(arg_to_validate))
-  validate:::parse_validator(quote(.), quote(arg_to_validate))
-  validate:::parse_validator(quote(. && a), quote(arg_to_validate))
-  validate:::parse_validator(quote(.(.)), quote(arg_to_validate))
-  validate:::parse_validator(quote(((a && b) || .(.))), quote(arg_to_validate))
-  validate:::parse_validator(quote(matrix(nrow=3)), quote(arg_to_validate))
-  validate:::parse_validator(quote(matrix(nrow=3) && .(.)), quote(arg_to_validate))
-  validate:::parse_validator(quote((a || ((b && c))) && .(a + .)), quote(arg_to_validate))
-  validate:::parse_validator(quote((a || ((b && .(c)))) && (a + .(.))), quote(arg_to_validate))
+  vetr:::parse_validator(quote(FALSE), quote(arg_to_validate))
+  vetr:::parse_validator(quote(((FALSE))), quote(arg_to_validate))
+  vetr:::parse_validator(quote(((FALSE && ((TRUE))))), quote(arg_to_validate))
+  vetr:::parse_validator(quote(.(FALSE)), quote(arg_to_validate))
+  vetr:::parse_validator(quote(.), quote(arg_to_validate))
+  vetr:::parse_validator(quote(. && a), quote(arg_to_validate))
+  vetr:::parse_validator(quote(.(.)), quote(arg_to_validate))
+  vetr:::parse_validator(quote(((a && b) || .(.))), quote(arg_to_validate))
+  vetr:::parse_validator(quote(matrix(nrow=3)), quote(arg_to_validate))
+  vetr:::parse_validator(quote(matrix(nrow=3) && .(.)), quote(arg_to_validate))
+  vetr:::parse_validator(quote((a || ((b && c))) && .(a + .)), quote(arg_to_validate))
+  vetr:::parse_validator(quote((a || ((b && .(c)))) && (a + .(.))), quote(arg_to_validate))
 
-  validate:::parse_validator(quote(a && (b + .(c))), quote(arg_to_validate))  # uninterpretable?
-  validate:::parse_validator(quote(a && .), "hello")                          # uninterpretable?
+  vetr:::parse_validator(quote(a && (b + .(c))), quote(arg_to_validate))  # uninterpretable?
+  vetr:::parse_validator(quote(a && .), "hello")                          # uninterpretable?
 } )
 unitizer_sect("token sub", {
-  validate:::symb_sub(INT.1)
-  validate:::symb_sub(NO.NA)
+  vetr:::symb_sub(INT.1)
+  vetr:::symb_sub(NO.NA)
 })
 
 unitizer_sect("preset tokens", {
   x <- quote(integer(1L))
   y <- quote(integer(1L) || NULL)
   z <- quote(integer(1L) && .(!any(is.na(.))))
-  validate:::parse_validator(quote(x), quote(w))
-  validate:::parse_validator(quote(y), quote(w))
-  validate:::parse_validator(quote(z), quote(w))
-  validate:::parse_validator(quote(z || NULL), quote(w))
+  vetr:::parse_validator(quote(x), quote(w))
+  vetr:::parse_validator(quote(y), quote(w))
+  vetr:::parse_validator(quote(z), quote(w))
+  vetr:::parse_validator(quote(z || NULL), quote(w))
 } )
 unitizer_sect("validators", {
-  validate:::parse_validator(INT.1, quote(w))
-  validate:::parse_validator(INT, quote(w))
-  validate:::parse_validator(CHR.1, quote(w))
-  validate:::parse_validator(CHR, quote(w))
-  validate:::parse_validator(NUM.1, quote(w))
-  validate:::parse_validator(NUM, quote(w))
-  validate:::parse_validator(LGL.1, quote(w))
-  validate:::parse_validator(LGL, quote(w))
-  validate:::parse_validator(CPX.1, quote(w))
-  validate:::parse_validator(CPX, quote(w))
+  vetr:::parse_validator(INT.1, quote(w))
+  vetr:::parse_validator(INT, quote(w))
+  vetr:::parse_validator(CHR.1, quote(w))
+  vetr:::parse_validator(CHR, quote(w))
+  vetr:::parse_validator(NUM.1, quote(w))
+  vetr:::parse_validator(NUM, quote(w))
+  vetr:::parse_validator(LGL.1, quote(w))
+  vetr:::parse_validator(LGL, quote(w))
+  vetr:::parse_validator(CPX.1, quote(w))
+  vetr:::parse_validator(CPX, quote(w))
 } )
