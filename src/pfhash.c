cb274c7281cb1b0881990aeb6c1a477634c19877
brodieG <brodieG@users.noreply.github.com>
2017-07-02 11:46:23 -0400

    add copyright notices

--- src/pfhash.c
+++ src/pfhash.c
@@ -1,6 +1,34 @@
 /*
 copyright paxdiablo
-lifted directly from: http://powerfield-software.com/?p=615
+lifted directly from: http://powerfield-software.com/?p=615 under license:
+
+> Brodie, the code I hold copyright for in this article (which is the bulk of
+> it) is covered by the “do whatever the heck you want with it” licence, the
+> official text of which is:
+>
+> 1/ You are hereby permitted to do whatever the heck you want with it.
+>
+> I make no representations about the actual hashing functions themselves,
+> defaultFnKnR and defaultFnBJ. If you want to ensure you’re safe in respect to
+> those, either consult a lawyer, write your own, or see the addendum to this
+> article.
+*/
+/*
+Copyright (C) 2017  Brodie Gaslam
+
+This file is part of "vetr - Trust, but Verify"
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+Go to <https://www.r-project.org/Licenses/GPL-2> for a copy of the license.
 */
 
 #include "pfhash.h"
