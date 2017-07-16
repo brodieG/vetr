a3959027c90ec2240463ce9109340306d0f33715
brodieG <brodieG@users.noreply.github.com>
2017-07-16 14:24:25 -0400

    docs

--- R/all-bw.R
+++ R/all-bw.R
@@ -6,9 +6,12 @@
 #' outside the range defined by `lo`-`hi`.  Keep in mind that betweeness of
 #' character strings will be affected by system collation, etc.
 #'
+#' If `x` and `lo`/`hi` are different types, `lo`/`hi` will be coerced to the
+#' type of `x`.  Coercions resulting in NA will trigger errors.
+#'
 #' @param x vector numeric, integer, or character
-#' @param lo scalar vector of conformable type to `x`
-#' @param hi scalar vector of conformable type to `x`
+#' @param lo scalar vector of type coercible to the type of `x`, cannot be NA
+#' @param hi scalar vector of type coercible to the type of `x`, cannot be NA
 #' @param na.rm TRUE or FALSE (default), whether NAs are allowed, NAs are
 #'   normally taken to never meet the `lo`-`hi` value requirements.
 #' @param include.ends `character(1L)` in:
