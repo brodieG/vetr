207c06baa1838d4209821b10d149032a3dad2959
brodieG <brodieG@users.noreply.github.com>
2017-07-16 14:25:26 -0400

    wip: fleshing out all_bw

--- src/all-bw.c
+++ src/all-bw.c
@@ -42,11 +42,94 @@ SEXP VALC_all_bw(
   // - Validation --------------------------------------------------------------
 
   if(xlength(hi) != 1)
-    error("Argument `hi` must be length 1 (is %.0f).", (double) xlength(hi))
+    error(
+      "Argument `hi` must be length 1 (is %s).", CSR_len_as_chr(xlength(hi))
+    );
   if(xlength(lo) != 1)
-    error("Argument `lo` must be length 1 (is %.0f).", (double) xlength(lo))
+    error(
+      "Argument `lo` must be length 1 (is %s).", CSR_len_as_chr(xlength(lo))
+    );
+  if(xlength(include_ends) != 1)
+    error(
+      "Argument `include.ends` must be length 1 (is %s).",
+      CSR_len_as_chr(xlength(include_ends))
+    );
+
+  if(scalar_na(hi)) error("Argument `hi` must not be NA.")
+  if(scalar_na(lo)) error("Argument `lo` must not be NA.")
+
+  if(TYPEOF(include_ends) != STRSXP || xlength(include_ends) != 1)
+    error(
+      "Argument `include.ends` must be character (is %s).",
+      type2char(TYPEOF(include_ends))
+    );
+  if(STRING_ELT(include_ends, 0) == NA_STRING)
+    error("Argument `include.ends` may not be NA.");
+
+  const char * inc_end_chr = CHAR(STRING_ELT(include_ends, 0));
+  int inc_lo = 0, inc_hi = 0;  // track whether to include bounds
+  int bw = 0;  // track whether doing inside or outside
+
+  if(CSR_strmlen(inc_end_chr, 3) != 2) include_end_err();
+
+  if(inc_end_chr[0] == '[' || inc_end_chr[0] == '(') {
+    bw = 1;
+  } else if (inc_end_chr[0] == ']' || inc_end_chr[0] == ')') {
+    bw = 0;
+  } else {
+    include_end_err();
+  }
+  if(inc_end_chr[1] == '[' || inc_end_chr[1] == '(') {
+    if(bw) include_end_err();
+  } else if (inc_end_chr[1] == ']' || inc_end_chr[1] == ')') {
+    if(!bw) include_end_err()
+  } else {
+    include_end_err();
+  }
+  inc_lo = inc_end_chr[0] == '[' || inc_end_chr[0] == ']'
+  inc_hi = inc_end_chr[1] == '[' || inc_end_chr[1] == ']'
+
+  // - Numerics ----------------------------------------------------------------
 
   if(num_like(x))  {
+    if(!num_like(lo))
+      error(
+        "Argument `x` is numeric-like, but `lo` is %s.", type2char(lo_type)
+      );
+    if(!num_like(hi))
+      error(
+        "Argument `x` is numeric-like, but `hi` is %s.", type2char(hi_type)
+      );
+
+    double lo_num = asReal(lo);
+    double hi_num = asReal(hi);
+    if(lo_num > hi_num) {
+      hi_num = lo_num;
+      lo_num = asReal(hi);
+    }
+
+
+    if(x_type == INTSXP) {
+      int lo_int = asInteger(lo);
+      int hi_int = asInteger(hi);
+
+      if(lo_int = NA_INTEGER)
+        error("Argument `lo` is NA when coerced to integer.");
+      if(hi_int = NA_INTEGER)
+        error("Argument `hi` is NA when coerced to integer.");
+
+      for(R_xlen_t i = 0; i < xlength(x); ++i) {
+        if(INTEGER(x)[i] > hi_int)
+          return ScalarString(
+            CSR_smprintf4(
+              10000, "only contain values less than %s (%s found at index %s)",
+              CSR_num_as_chr(hi, 1), CSR_num_as_chr(INTEGER(x)[i], 1),
+              CSR_len_as_chr(i, 1)
+            )
+          );
+      }
+    } else if (x_type == REALSXP) {
+    } else error("Internal Error: invalid `x` type %s.", type2char(x_type));
 
   } else if(x_type == STRSXP) {
 
@@ -56,9 +139,5 @@ SEXP VALC_all_bw(
       type2char(x_type)
     );
   }
-  if(!num_like(lo))
-    error("Argument `x` is numeric-like, `lo` is %s.", type2char(lo_type));
-  if(!num_like(hi))
-    error("Argument `x` is numeric-like, `hi` is %s.", type2char(hi_type));
   return R_NilValue;
 }
