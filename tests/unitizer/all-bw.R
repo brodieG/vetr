e257c5c75839ca108d4ed1a4c9f51783c0228037
brodieG <brodieG@users.noreply.github.com>
2017-07-18 10:51:41 -0400

    more tests

--- tests/unitizer/all-bw.R
+++ tests/unitizer/all-bw.R
@@ -1,9 +1,10 @@
 library(vetr)
 
+x <- runif(100)
+x[1] <- 1
+x[2] <- 0
+
 unitizer_sect('all_bw', {
-  x <- runif(100)
-  x[1] <- 1
-  x[2] <- 0
   all_bw(x, 0, 1)
   all_bw(x, 0, 1, include.bounds="[)") # fail
   all_bw(x, 0, 1, include.bounds="(]") # fail
@@ -22,4 +23,50 @@ unitizer_sect('all_bw', {
 
   all_bw(z, 0, 1)              # fail
   all_bw(z, 0, 1, na.rm=TRUE)  # pass
+})
+unitizer_sect('exclude', {
+  all_bw(x, 1 + 1e-6, 2, include.bounds="][") # pass
+  all_bw(x, 1, 2, include.bounds="][")        # fail
+  all_bw(x, 1, 2, include.bounds=")[")        # fail
+  all_bw(x, 1, 2, include.bounds=")(")        # pass
+  all_bw(x, 1, 2, include.bounds="](")        # pass
+
+  all_bw(x, -1, 0 - 1e-6, include.bounds="][")# pass
+  all_bw(x, -1, 0, include.bounds="][")       # fail
+  all_bw(x, -1, 0, include.bounds="](")       # pass
+  all_bw(x, -1, 0, include.bounds=")[")       # fail
+  all_bw(x, -1, 0, include.bounds=")(")       # pass
+})
+unitizer_sect('corner cases', {
+  all_bw(x, 0, 0)                        # fail
+  all_bw(x, 0, 0, include.bounds=")(")   # pass
+
+})
+unitizer_sect('Infinity', {
+  z <- w <- runif(100, 1e-100, 1e100)
+
+  all_bw(z, -Inf, Inf)  # pass
+
+  w[50] <- -Inf
+  w[51] <- Inf
+
+  all_bw(w, -Inf, Inf)        # pass?
+  all_bw(w, -Inf, Inf, "[)")  # fail
+  all_bw(w, -Inf, Inf, "(]")  # fail
+
+  all_bw(w, -Inf, Inf, ")(")  # pass
+  all_bw(w, -Inf, Inf, ")[")  # fail
+  all_bw(w, -Inf, Inf, "](")  # fail
+  all_bw(w, -Inf, Inf, "][")  # fail
+})
+unitizer_sect('errors', {
+  all_bw(x, 0, -1)
+  all_bw(x, -1, 1, na.rm=1)
+  all_bw(x, -1, 1, na.rm=c(TRUE, FALSE))
+  all_bw(x, -1, 1, na.rm=NA)
+
+  all_bw(x, -1, 1, include.bounds=TRUE)
+  all_bw(x, -1, 1, include.bounds=letters)
+  all_bw(x, -1, 1, include.bounds="[[")
+  all_bw(x, -1, 1, include.bounds="))")
 })
