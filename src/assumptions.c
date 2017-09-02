4afbadb6f502b0025c88c2d14bd410f1479dce2b
brodieG <brodieG@users.noreply.github.com>
2017-09-02 14:17:20 -0400

    change SIZE_T_MAX to SIZE_MAX

--- src/assumptions.c
+++ src/assumptions.c
@@ -62,8 +62,8 @@ SEXP VALC_check_assumptions() {
   // Because we check that strings are no longer than this, but then allocate
   // memory as INT_MAX + 1 with a size_t, so need to make sure that fits
 
-  if(SIZE_T_MAX - 1 < INT_MAX)
-    error(err_base, "SIZE_T_MAX not sufficiently larger than INT_MAX", "");
+  if(SIZE_MAX - 1 < INT_MAX)
+    error(err_base, "SIZE_MAX not sufficiently larger than INT_MAX", "");
   return ScalarLogical(1);
 }
 // nocov end
