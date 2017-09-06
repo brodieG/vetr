82bc1327fee56d3c689a606429b9358690cbe6e4
brodieG <brodieG@users.noreply.github.com>
2017-09-05 20:28:35 -0400

    env vars for compile checks

--- tests/run.R
+++ tests/run.R
@@ -1,6 +1,5 @@
 library(unitizer)
-
-# R_COMPILE_PKGS=1
-# R_JIT_STRATEGY=4
-# R_CHECK_CONSTANTS=5
+# env R_COMPILE_PKGS=1 R CMD build .
+# env R_JIT_STRATEGY=4 R_CHECK_CONSTANTS=5 R CMD check vetr_0.1.0.9008.tar.gz
+# env R_COMPILE_PKGS=1 R_JIT_STRATEGY=4 R_CHECK_CONSTANTS=5 R
 unitize_dir('unitizer', state='recommended')
