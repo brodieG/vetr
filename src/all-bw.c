255abdf97c500d829f67f0cc0b3146da5c348831
brodieG <brodieG@users.noreply.github.com>
2017-09-10 20:17:38 -0400

    unprotected objs in all_bw

--- src/all-bw.c
+++ src/all-bw.c
@@ -507,8 +507,8 @@ SEXP VALC_all_bw(
     // complicated.  Also, we really only need this in failure but we're always
     // building it...
 
-    SEXP lo_chr_sxp = asChar(lo);
-    SEXP hi_chr_sxp = asChar(hi);
+    SEXP lo_chr_sxp = PROTECT(asChar(lo));
+    SEXP hi_chr_sxp = PROTECT(asChar(hi));
     lo_chr = CHAR(lo_chr_sxp);
     hi_chr = CHAR(hi_chr_sxp);
 
@@ -527,7 +527,7 @@ SEXP VALC_all_bw(
       CSR_num_as_chr(asReal(hi), 0) :
       CSR_smprintf2(10000L, "\"%s\"", CHAR(STRING_ELT(hilo_string_sub, 1)), "");
 
-    UNPROTECT(4);
+    UNPROTECT(6);
 
     if(!lo_unbound && !hi_unbound && strcmp(lo_chr, hi_chr) > 0) {
       error(
