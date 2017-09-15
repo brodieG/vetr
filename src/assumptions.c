d440180fa704a168aed7389476538d54dc1a6109
brodieG <brodieG@users.noreply.github.com>
2017-09-14 22:53:09 -0400

    assumption warn not err, relax size_t req

--- src/assumptions.c
+++ src/assumptions.c
@@ -33,46 +33,54 @@ Go to <https://www.r-project.org/Licenses/GPL-2> for a copy of the license.
 SEXP VALC_check_assumptions() {
   const char * err_base = "Failed system assumption: %s%s";
   if(sizeof(R_len_t) < sizeof(int))
-    error(err_base, "R_len_t is not gte to int", "");
+    warningcall(R_NilValue, err_base, "R_len_t is not gte to int", "");
 
   // Otherwise bit twiddling assumptions may not work as expected?
 
-  if(CHAR_BIT != 8) error(err_base, "CHAR_BIT is not 8", "");
+  if(CHAR_BIT != 8)
+    warningcall(R_NilValue, err_base, "CHAR_BIT is not 8", "");
 
   // This is supposedly enforced by R
 
-  if(sizeof(int) < 4) error(err_base, "ints are not at least 32 bits", "");
+  if(sizeof(int) < 4)
+    warningcall(R_NilValue, err_base, "ints are not at least 32 bits", "");
 
   // If this is not TRUE, there could be alignment issues for some of our
   // structs that use size_t elements given that R_alloc only guarantees double
-  // alignment.  This requirements is too strict as written; really we need
-  // size_t to be LTE double provided that 2^n * size_t == double.
+  // alignment.
+  //
+  // This will likely cause problems on systems other than 32 and 64 bits,
+  // particularly those with larger register sizes, probably the easiest
+  // solution is to not use size_t in the structs if this becomes a problem
 
-  if(sizeof(size_t) != sizeof(double))
-    error(err_base, "size_t and double not same size");
+  if(sizeof(size_t) > sizeof(double))
+    warningcall(R_NilValue, err_base, "size_t larger than double not same size");
 
   // Important for some our boundary condition assumptions, in particular that
   // NA_INTEGER < int x.
 
   if(INT_MIN != NA_INTEGER) {
-    error(
-      err_base,"INT_MIN != NA_INTEGER but the code in this ",
+    warningcall(
+      R_NilValue, err_base, "INT_MIN != NA_INTEGER but the code in this ",
       "package assumes that they are equal; please contact maintainer."
     );
   }
   // Mostly because we try to represent R_xlen_t values with %.0f
 
   if(R_XLEN_T_MAX >= DBL_MAX)
-    error(err_base, "R_XLEN_T_MAX is not less than DBL_MAX");
+    warningcall(R_NilValue, err_base, "R_XLEN_T_MAX is not less than DBL_MAX");
 
   if(sizeof(R_len_t) != sizeof(int))
-    error(err_base, "R_len_t not same size as int", "");
+    warningcall(R_NilValue, err_base, "R_len_t not same size as int", "");
 
   // Because we check that strings are no longer than this, but then allocate
   // memory as INT_MAX + 1 with a size_t, so need to make sure that fits
 
   if(SIZE_MAX - 1 < INT_MAX)
-    error(err_base, "SIZE_MAX not sufficiently larger than INT_MAX", "");
+    warningcall(
+      R_NilValue, err_base,
+      "SIZE_MAX not sufficiently larger than INT_MAX", ""
+    );
   return ScalarLogical(1);
 }
 // nocov end
