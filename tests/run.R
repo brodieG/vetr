6b485b04ba62bf0b80ec52df7926526c0f2b56b0
brodieG <brodieG@users.noreply.github.com>
2020-05-12 21:33:38 -0400

    stringsAsFactors
    Also, some weirdness with rlang in the ggplot example
    where we got warnings at some point that just went away

--- tests/run.R
+++ tests/run.R
@@ -2,10 +2,8 @@ cat(getwd(), "\n")
 if(suppressWarnings(require('unitizer'))) {
   local({
     suppressWarnings(RNGversion("3.5.2"));
-    old.opt <- options(stringsAsFactors=TRUE)
     on.exit({
       RNGversion(as.character(getRversion()))
-      options(old.opt)
     })
     pattern <- 'alike'
     unitize_dir(
