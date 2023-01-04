ddaad4e90cc1145beb13bef118bda75461b291a3
brodieG <brodieG@users.noreply.github.com>
2023-01-04 06:57:03 -0500

    update copyright year

--- R/type.R
+++ R/type.R
@@ -1,4 +1,4 @@
-# Copyright (C) 2022 Brodie Gaslam
+# Copyright (C) 2023 Brodie Gaslam
 #
 # This file is part of "vetr - Trust, but Verify"
 #
--- src/valname.c
+++ src/valname.c
@@ -1,5 +1,5 @@
 /*
-Copyright (C) 2020 Brodie Gaslam
+Copyright (C) 2023 Brodie Gaslam
 
 This file is part of "vetr - Trust, but Verify"
 
